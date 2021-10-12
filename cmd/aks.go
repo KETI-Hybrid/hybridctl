fae14119fff11c3faf62821c5b556d6135ca057d
root <root@127.0.1.1>
2021-10-12 19:13:10 +0900

    backup

--- cmd/aks.go
+++ cmd/aks.go
@@ -28,6 +28,10 @@ var aksCmd = &cobra.Command{
 }
 
 func init() {
-
+	// RootCmd.AddCommand(aksCmd)
+	// aksCmd.AddCommand(StartCmd)
+	// aksCmd.AddCommand(StopCmd)
+	// aksCmd.AddCommand(RotateCertsCmd)
+	// aksCmd.AddCommand(GetOSoptionsCmd)
 	aksFlags()
 }
