ee9526aef9058f54476010c92ad5d0e52900f1c0
root <root@127.0.1.1>
2021-10-13 10:22:42 +0900

    test1

--- cmd/aks.go
+++ cmd/aks.go
@@ -15,6 +15,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -33,5 +35,6 @@ func init() {
 	aksCmd.AddCommand(StopCmd)
 	aksCmd.AddCommand(RotateCertsCmd)
 	aksCmd.AddCommand(GetOSoptionsCmd)
+	fmt.Printf("1")
 	aksFlags()
 }
