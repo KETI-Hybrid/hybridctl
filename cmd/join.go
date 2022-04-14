e0bf3ed3f1034f2ae9ace2210b36fc64afff11f0
lcw729 <lcw729729@gmail.com>
2022-04-14 17:58:50 +0900

    remove platform arguments & hybridctl join register cli

--- cmd/join.go
+++ cmd/join.go
@@ -16,13 +16,9 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 
-	resource "Hybrid_Cloud/kube-resource/namespace"
-	hcpclusterapis "Hybrid_Cloud/pkg/apis/hcpcluster/v1alpha1"
 	hcpclusterv1alpha1 "Hybrid_Cloud/pkg/client/hcpcluster/v1alpha1/clientset/versioned"
-	u "Hybrid_Cloud/util"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
@@ -36,91 +32,26 @@ var joinCmd = &cobra.Command{
 	Use:   "join",
 	Short: "A brief description of your command",
 	Long: ` 
-NAME 
-	hybridctl join PLATFORM CLUSTER
-	hybridctl join register PLATFORM
+	NAME 
+		hybridctl join CLUSTER
 
-DESCRIPTION
+	DESCRIPTION
 	
-	>> cluster join PLATFORM CLUSTER <<
-
-
-	PLATFORM means the Kubernetes platform of the cluster to join.
-	The types of platforms offered are as follows.
-
-	- aks   azure kubernetes service
-	- eks   elastic kubernetes service
-	- gke   google kuberntes engine
-
-	* PLATFORM mut be written in LOWERCASE letters
+	>> cluster join CLUSTER <<
 
 	CLUSTER means the name of the cluster on the specified platform.
-
-	>> hybridctl join register PLATFORM <<
-
-	* This command registers the cluster you want to manage, 
-	For each platform, you must fill in the information below.
-	Please refer to the INFO section
-
-	PLATFORM means the Kubernetes platform of the cluster to join.
-	The types of platforms offered are as follows.
-
-	- aks   azure kubernetes service
-	  hybridctl join register aks CLUSTER_NAME RESOURCEGROUP
-
-	- eks   elastic kubernetes service
-	  hybridctl join register eks CLUSTER_NAME
-
-	- gke   google kuberntes engine
-	  hybridctl join register gke CLUSTER_NAME 
 
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
 
 		if len(args) == 0 {
+			fmt.Println(cmd.Help())
 		} else {
-			switch args[0] {
-			case "aks":
-				fallthrough
-			case "eks":
-				fallthrough
-			case "gke":
-				fmt.Println("kubernetes engine Name : ", args[0])
-				fmt.Printf("Cluster Name : %s\n", args[1])
-				// platform := args[0]
-				clustername := args[1]
-				if !CheckHCPClusterListToJoin(clustername) {
-					return
-				}
-			case "register":
-				platform := args[1]
-				if platform == "" {
-					fmt.Println("ERROR: Input Platform")
-				}
-				clustername := args[2]
-				if clustername == "" {
-					fmt.Println("ERROR: Input Clustername")
-				}
-				resource.CheckAndCreateNamespace("kube-master", HCP_NAMESPACE)
-				switch platform {
-				case "aks":
-					fallthrough
-				case "eks":
-					fallthrough
-				case "gke":
-					if CreateHCPCluster(platform, clustername) {
-						err := u.ChangeConfigClusterName(HCP_NAMESPACE, clustername)
-						if err != nil {
-							fmt.Println(err)
-						}
-					}
-					return
-				default:
-					return
-				}
-			default:
-				fmt.Println("Run 'hybridctl join --help' to view all commands")
+			fmt.Printf("Cluster Name : %s\n", args[0])
+			clustername := args[0]
+			if !CheckHCPClusterListToJoin(clustername) {
+				return
 			}
 		}
 	},
@@ -134,18 +65,15 @@ func CheckHCPClusterListToJoin(clustername string) bool {
 	cluster_list, err := hcp_cluster.HcpV1alpha1().HCPClusters(HCP_NAMESPACE).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		log.Println(err)
+		return false
 	}
 
 	for _, cluster := range cluster_list.Items {
 		joinstatus := cluster.Spec.JoinStatus
-		fmt.Println(cluster.Spec.ClusterPlatform)
-		fmt.Println(cluster.Name)
-		// if cluster.Spec.ClusterPlatform == platform && cluster.Name == clustername {
 		if cluster.Name == clustername {
 			if joinstatus == "UNJOIN" {
 				cluster.Spec.JoinStatus = "JOINING"
 				_, err = hcp_cluster.HcpV1alpha1().HCPClusters(HCP_NAMESPACE).Update(context.TODO(), &cluster, metav1.UpdateOptions{})
-				fmt.Println(cluster.Spec.JoinStatus)
 				if err != nil {
 					fmt.Println(err)
 					return false
@@ -162,53 +90,11 @@ func CheckHCPClusterListToJoin(clustername string) bool {
 				return false
 			}
 		}
-		fmt.Println("?")
 	}
 	fmt.Println("ERROR: no such Cluster")
 	fmt.Println("you must register yout cluster to join")
 	fmt.Println("ex) kubectl register <platform> <clustername>")
 	return false
-}
-
-func CreateHCPCluster(platform string, clustername string) bool {
-	hcp_cluster, err := hcpclusterv1alpha1.NewForConfig(master_config)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-	data, err := ioutil.ReadFile("/root/.kube/kubeconfig")
-	if err != nil {
-		fmt.Println("File reading error", err)
-		return false
-	}
-	if data == nil {
-		fmt.Printf("fail to get config about %s\n", clustername)
-		return false
-	}
-	cluster := hcpclusterapis.HCPCluster{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "HCPCluster",
-			APIVersion: "hcp.crd.com",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      clustername,
-			Namespace: HCP_NAMESPACE,
-		},
-		Spec: hcpclusterapis.HCPClusterSpec{
-			ClusterPlatform: platform,
-			KubeconfigInfo:  data,
-			JoinStatus:      "UNJOIN",
-		},
-	}
-	newhcpcluster, err := hcp_cluster.HcpV1alpha1().HCPClusters(HCP_NAMESPACE).Create(context.TODO(), &cluster, metav1.CreateOptions{})
-	// err = hcp_cluster.HcpV1alpha1().HCPClusters(platform).Delete(context.TODO(), clustername, metav1.DeleteOptions{})
-	if err != nil {
-		fmt.Println(err)
-		return false
-	} else {
-		fmt.Printf("success to register %s in %s\n", newhcpcluster.Name, newhcpcluster.Namespace)
-		return true
-	}
 }
 
 func init() {
